d48f074b14835bb31131c73c5c9130df6655e22f
gregoria-i <gm.andrea121@gmail.com>
2026-04-09 03:46:41 -0600

    Cambio sobre la barra de progreso falsa

--- script.js
+++ script.js
@@ -3,28 +3,29 @@ function mostrarCuestionario() {
   document.getElementById("cuestionario").classList.remove("hidden");
 }
 
-function procesar() {
-
-  document.getElementById("cuestionario").classList.add("hidden");
-  document.getElementById("resultado").classList.remove("hidden");
-
-  let progreso = 0;
-  let barra = document.getElementById("barra");
-
-  let intervalo = setInterval(() => {
-    progreso += 10;
-    barra.style.width = progreso + "%";
-
-    if (progreso >= 100) {
-      clearInterval(intervalo);
-    }
-  }, 200);
-}
 function irAResultados() {
 
   window.open(
     "https://docs.google.com/forms/d/e/1FAIpQLSeavbJumqAzK0jd10roLrfw-6Cahvw-6sPSoWxsQol6ifzaQA/viewform",
     "_blank"
   );
-procesar();
+
+  // 2. Cambiar a pantalla 3
+  document.getElementById("cuestionario").classList.add("hidden");
+  document.getElementById("resultado").classList.remove("hidden");
+
+  // 3. Animación
+  let progreso = 0;
+  let barra = document.getElementById("barra");
+
+  let intervalo = setInterval(() => {
+    progreso += 20;
+    barra.style.width = progreso + "%";
+
+    if (progreso >= 100) {
+      clearInterval(intervalo);
+
+      document.getElementById("contenido").classList.remove("hidden");
+    }
+  }, 100);
 }
